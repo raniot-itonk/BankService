2363e4e1e2eb6589ce64abefe48b8c27591eefff
Anders Meidahl <Andersmeidahl@gmail.com>
2019-02-13 16:51:29 +0100

    Created first take on BankService, with auto generated controller

---
+++ BankService/Controllers/AccountsController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankService.DB;
+
+namespace BankService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly BankingContext _context;
+
+        public AccountsController(BankingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+        {
+            return await _context.Accounts.ToListAsync();
+        }
+
+        // GET: api/Accounts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetAccount(string id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return account;
+        }
+
+        // PUT: api/Accounts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccount(string id, Account account)
+        {
+            if (id != account.OwnerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Accounts
+        [HttpPost]
+        public async Task<ActionResult<Account>> PostAccount(Account account)
+        {
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAccount", new { id = account.OwnerId }, account);
+        }
+
+        // DELETE: api/Accounts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Account>> DeleteAccount(string id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+
+            return account;
+        }
+
+        private bool AccountExists(string id)
+        {
+            return _context.Accounts.Any(e => e.OwnerId == id);
+        }
+    }
+}
---
+++ BankService/DB/BankingContext.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankService.DB
+{
+    public class BankingContext : DbContext
+    {
+        public BankingContext(DbContextOptions<BankingContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Account> Accounts { get; set; }
+    }
+
+    public class Account
+    {
+        [Key]
+        public string OwnerId { get; set; }
+        [MaxLength(100)]
+        public string OwnerName { get; set; }
+        public double Balance { get; set; }
+    }
+}
